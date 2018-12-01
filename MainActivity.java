275de40940e404abe13bd799e9fcf4e96ff9c858
layanb98 <44979799+layanb98@users.noreply.github.com>
2018-12-01 14:57:07 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,57 +1,131 @@
-package wavegen.wavegengame;
+package com.example.layanbahaidarah.firstapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    //creating the seek bars and the texts that would have their values
+    private static SeekBar W1;
+    private static TextView T1;
+    private static Double V1;
+    private static SeekBar W2;
+    private static TextView T2;
+    private static Double V2;
+    private static SeekBar W3;
+    private static TextView T3;
+    private static Double V3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        the1Wave();
+        the2Wave();
+        the3Wave();
+        //playerOutput();
+    }
 
-        launchMenu();
 
+    //testing how to make the first wave
+    public void the1Wave() {
+        W1 = (SeekBar) findViewById(R.id.wave1Bar);
+        T1 = (TextView) findViewById(R.id.wave1Text);
 
+        W1.setOnSeekBarChangeListener(
+
+                new SeekBar.OnSeekBarChangeListener() {
+                    double value1;
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        value1 = (progress * 2 * Math.PI) / 100;
+                        V1 = (int) Math.round(value1 * 100) / (double) 100;
+                        T1.setText("W1 " + V1);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        T1.setText("W1 " + V1);
+                        playerOutput();
+                    }
+                }
+        );
 
     }
 
-    private void launchMenu(){
-        Intent menuActivity = new Intent(MainActivity.this, Menu.class);
+    //second wave
+    public void the2Wave() {
+        W2 = (SeekBar) findViewById(R.id.wave2Bar);
+        T2 = (TextView) findViewById(R.id.wave2Text);
+        W2.setOnSeekBarChangeListener(
 
-        startActivityForResult(menuActivity, 1);
+                new SeekBar.OnSeekBarChangeListener() {
+
+                    double value2;
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        value2 = (progress * 2 * Math.PI) / 100;
+                        V2 = (int) Math.round(value2 * 100) / (double) 100;
+                        T2.setText("W2 " + V2);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        T2.setText("W2 " + V2);
+                        playerOutput();
+                    }
+                }
+        );
     }
 
-    private void launchGame(){
-        Intent gameActivity = new Intent(MainActivity.this, Game.class);
 
-        startActivityForResult(gameActivity, 2);
+    public void the3Wave() {
+        W3 = (SeekBar) findViewById(R.id.wave3Bar);
+        T3 = (TextView) findViewById(R.id.wave3Text);
+        W3.setOnSeekBarChangeListener(
+
+                new SeekBar.OnSeekBarChangeListener() {
+
+                    double value3;
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        value3 = (progress * 2 * Math.PI) / 100;
+                        V3 = (int) Math.round(value3 * 100) / (double) 100;
+                        T3.setText("W3 " + V3);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        T3.setText("W3 " + V3);
+                        playerOutput();
+                    }
+                }
+        );
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            if(resultCode == RESULT_OK) {
-                String strEditText = data.getStringExtra("button_pressed");
-            }
-        }
-        if (requestCode == 2) {
-            if (resultCode == RESULT_OK) {
-                String strEditText = data.getStringExtra("editTextValue");
-            }
-        }
+    public void playerOutput() {
+        TextView allWaves = findViewById(R.id.allWavesText);
+        allWaves.setText(V1 + " " + V2 + " " + V3);
     }
 }
+
