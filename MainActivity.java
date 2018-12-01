a2c745ec4f942186d53f24959f14a0a2bd5c154e
Myles-Cork <38143953+Myles-Cork@users.noreply.github.com>
2018-12-01 14:34:58 -0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,57 @@
+package wavegen.wavegengame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        launchMenu();
+
+
+
+    }
+
+    private void launchMenu(){
+        Intent menuActivity = new Intent(MainActivity.this, Menu.class);
+
+        startActivityForResult(menuActivity, 1);
+    }
+
+    private void launchGame(){
+        Intent gameActivity = new Intent(MainActivity.this, Game.class);
+
+        startActivityForResult(gameActivity, 2);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK) {
+                String strEditText = data.getStringExtra("button_pressed");
+            }
+        }
+        if (requestCode == 2) {
+            if (resultCode == RESULT_OK) {
+                String strEditText = data.getStringExtra("editTextValue");
+            }
+        }
+    }
+}
---
+++ Menu.java
@@ -0,0 +1,36 @@
+package wavegen.wavegengame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Menu extends Activity implements View.OnClickListener {
+    public static final String PLAY_BUTTON = "Play";
+
+    private Button play_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        play_button = (Button) findViewById(R.id.playB);
+        play_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId()=R.id.playB){
+            buttonPressed(true);
+        }
+    }
+
+    private void buttonPressed(boolean button){
+        Intent result = new Intent();
+        result.putExtra("button_pressed", button);
+        setResult(RESULT_OK, result);
+        finish();
+    }
+}
