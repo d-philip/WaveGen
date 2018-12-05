9667e3bc749bfbda625c83ccf8a798f5bdc4a0f6
layanbahaidarah <layanb@bu.edu>
2018-12-04 21:21:32 -0500

    I changed everything it's more like kerflux now

--- app/src/main/java/wavegen/wavegengame/MainActivity.java
+++ app/src/main/java/wavegen/wavegengame/MainActivity.java
@@ -1,9 +1,14 @@
 package wavegen.wavegengame;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Random;
 
@@ -12,45 +17,40 @@ import wavegen.wavegengame.R;
 public class MainActivity extends AppCompatActivity {
 
     //creating the seek bars and the texts that would have their values
-    private static SeekBar W1;
-    private static TextView T1;
-    private static Double V1=0.0;
-    private static SeekBar W2;
-    private static TextView T2;
-    private static Double V2=0.0;
-    private static SeekBar W3;
-    private static TextView T3;
-    private static Double V3=0.0;
-    double rangeMin=0.0;
-    double rangeMax=Math.PI*3;
-    double random = new Random().nextDouble();
-    double randomValue = (int) Math.round((rangeMin + (random*(rangeMax - rangeMin)))* 100) / (double) 100;
+    private static SeekBar W1,W2;
+    private static TextView T1,T2;
+    private static Integer V1 =0,V2=0 ;
+    private static Double[] points1,points2,total,user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         the1Wave();
         the2Wave();
-        the3Wave();
+        randomizing();
+        initialGraphs();
     }
-
 
     //testing how to make the first wave
     public void the1Wave() {
-        W1 = (SeekBar) findViewById(R.id.wave1Bar);
-        T1 = (TextView) findViewById(R.id.wave1Text);
+        W1 = findViewById(R.id.wave1Bar);
+        T1 = findViewById(R.id.wave1Text);
+
 
         W1.setOnSeekBarChangeListener(
 
                 new SeekBar.OnSeekBarChangeListener() {
-                    double value1;
+
 
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        value1 = (progress * 2 * Math.PI) / 100;
-                        V1 = (int) Math.round(value1 * 100) / (double) 100;
+                        V1 = progress;
                         T1.setText("W1 " + V1);
+                        userGraphs();
+                        success();
+//                        playerOutput();
                     }
 
                     @Override
@@ -61,29 +61,33 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onStopTrackingTouch(SeekBar seekBar) {
                         T1.setText("W1 " + V1);
-                        playerOutput();
-                        success();
+//                        playerOutput();
+
                     }
                 }
         );
+
 
     }
 
     //second wave
     public void the2Wave() {
-        W2 = (SeekBar) findViewById(R.id.wave2Bar);
-        T2 = (TextView) findViewById(R.id.wave2Text);
+        W2 = findViewById(R.id.wave2Bar);
+        T2 = findViewById(R.id.wave2Text);
         W2.setOnSeekBarChangeListener(
 
                 new SeekBar.OnSeekBarChangeListener() {
 
-                    double value2;
+
 
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        value2 = (progress * 2 * Math.PI) / 100;
-                        V2 = (int) Math.round(value2 * 100) / (double) 100;
+
+                        V2= progress;
                         T2.setText("W2 " + V2);
+                        userGraphs();
+                        success();
+                        playerOutput();
                     }
 
                     @Override
@@ -95,57 +99,184 @@ public class MainActivity extends AppCompatActivity {
                     public void onStopTrackingTouch(SeekBar seekBar) {
                         T2.setText("W2 " + V2);
                         playerOutput();
-                        success();
+
                     }
                 }
         );
     }
 
-
-    public void the3Wave() {
-        W3 = (SeekBar) findViewById(R.id.wave3Bar);
-        T3 = (TextView) findViewById(R.id.wave3Text);
-        W3.setOnSeekBarChangeListener(
-
-                new SeekBar.OnSeekBarChangeListener() {
-
-                    double value3;
-
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        value3 = (progress * 2 * Math.PI) / 100;
-                        V3 = (int) Math.round(value3 * 100) / (double) 100;
-                        T3.setText("W3 " + V3);
-                    }
-
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-
-                    }
-
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-                        T3.setText("W3 " + V3);
-                        playerOutput();
-                        success();
-                    }
-                }
-        );
-    }
 
     public void playerOutput() {
         TextView allWaves = findViewById(R.id.allWavesText);
-        allWaves.setText(V1 + " " + V2 + " " + V3);
+        allWaves.setText(V1 + " " + V2);
     }
     public void success()
     {
-        double max=V1+V2+V3+0.05;
-        double min=V1+V2+V3-0.05;
-        TextView Rtext = findViewById(R.id.RandomText);
-        if(randomValue<=max && randomValue>=min)
-            Rtext.setText("You did it!");
+        double max,min;
+        int count=0;
+        for (int i=0;i<20;i++)
+        {
+            max = (points1[i] + points2[i]) + 2.0;
+            min = (points1[i] + points2[i]) - 2.0;
 
-        else Rtext.setText("Try Again" );
+            if(user[i]<=max && user[i]>=min)
+                count++;
+        }
+        TextView rest = findViewById(R.id.RandomText);
+
+        if(count==20)
+            rest.setText("You did it!");
+
+        else rest.setText("Try Again");
+    }
+    public void randomizing()
+    {
+        double Min=-1.0;
+        double Max=1.0;
+        points1=new Double[20];
+        points2=new Double[20];
+        total=new Double[20];
+        double r1,r2;
+
+        for(int i=0; i<20;i++)
+        {
+            r1=new Random().nextDouble();
+            r2=new Random().nextDouble();
+            points1[i]= (int) Math.round((Min + (r1*(Max - Min)))* 100) / (double) 100;
+            points2[i]= (int) Math.round((Min + (r2*(Max - Min)))* 100) / (double) 100;
+            total[i]=points1[i]+points2[i];
+        }
+
+    }
+    //
+    public void initialGraphs()
+    {
+        GraphView graph1 = findViewById(R.id.graph1);
+        GraphView graph2 = findViewById(R.id.graph2);
+        GraphView graph3 = findViewById(R.id.graph3);
+        GraphView graph4 = findViewById(R.id.graph4);
+
+        graph1.getGridLabelRenderer().setGridColor(0);
+        graph1.getGridLabelRenderer().setHorizontalLabelsColor(0);
+        graph1.getGridLabelRenderer().setVerticalLabelsColor(0);
+        graph2.getGridLabelRenderer().setGridColor(0);
+        graph2.getGridLabelRenderer().setHorizontalLabelsColor(0);
+        graph2.getGridLabelRenderer().setVerticalLabelsColor(0);
+        graph3.getGridLabelRenderer().setGridColor(0);
+        graph3.getGridLabelRenderer().setHorizontalLabelsColor(0);
+        graph3.getGridLabelRenderer().setVerticalLabelsColor(0);
+        graph4.getGridLabelRenderer().setGridColor(0);
+        graph4.getGridLabelRenderer().setHorizontalLabelsColor(0);
+        graph4.getGridLabelRenderer().setVerticalLabelsColor(0);
+
+
+        LineGraphSeries<DataPoint> series1 = new LineGraphSeries<DataPoint>();
+        double p;
+
+        for(int i = 0; i<points1.length; i++)
+        {
+            p= points1[i];
+            series1.appendData(new DataPoint(i,p),true,20);
+        }
+
+        graph1.addSeries(series1);
+
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<DataPoint>();
+        double p2;
+
+        for(int i = 0; i<points2.length; i++)
+        {
+            p2= points2[i];
+            series2.appendData(new DataPoint(i,p2),true,20);
+        }
+
+        graph2.addSeries(series2);
+
+        LineGraphSeries<DataPoint> series3 = new LineGraphSeries<DataPoint>();
+        double t;
+
+        for(int i = 0; i<total.length; i++)
+        {
+            t= total[i];
+            series3.appendData(new DataPoint(i ,t),true,20);
+        }
+
+        graph3.addSeries(series3);
+
+    }
+
+    public void userGraphs()
+    {
+        GraphView graph1 = findViewById(R.id.graph1);
+        GraphView graph2 = findViewById(R.id.graph2);
+        GraphView graph4 = findViewById(R.id.graph4);
+
+        graph1.removeAllSeries();
+        graph2.removeAllSeries();
+        graph4.removeAllSeries();
+
+        user = new Double[20];
+        double temp1,temp2;
+        double[] p1=new double [20];
+        double[] p2=new double [20];
+
+        for(int i =0; i<points1.length; i++)
+        {
+            p1[i]=points1[i];
+            p2[i]=points2[i];
+        }
+
+        for(int j=V1; j > 0 ; j--) {
+            temp2=p1[0];
+            for (int i = 1; i < 20; i++) {
+                temp1 = p1[i];
+                p1[i-1] = p1[i];
+                p1[i] = temp1;
+            }
+            p1[19]=temp2;
+        }
+        for(int j=V2; j > 0 ; j--) {
+            temp2=p2[0];
+            for (int i = 1; i < 20; i++) {
+                temp1 = p2[i];
+                p2[i-1] = p2[i];
+                p2[i] = temp1;
+            }
+            p2[19]=temp2;
+        }
+
+        LineGraphSeries<DataPoint> series1 = new LineGraphSeries<DataPoint>();
+        double p;
+
+        for(int i = 0; i<p1.length; i++)
+        {
+            p= p1[i];
+            series1.appendData(new DataPoint(i,p),true,20);
+        }
+
+        graph1.addSeries(series1);
+
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<DataPoint>();
+
+        for(int i = 0; i<p2.length; i++)
+        {
+            p= p2[i];
+            series2.appendData(new DataPoint(i,p),true,20);
+        }
+
+        graph2.addSeries(series2);
+
+        LineGraphSeries<DataPoint> series4 = new LineGraphSeries<DataPoint>();
+        series4.setColor(Color.RED);
+
+        for(int i = 0; i<user.length; i++)
+        {
+            user[i]=p1[i]+p2[i];
+            p= user[i];
+            series4.appendData(new DataPoint(i,p),true,20);
+        }
+
+        graph4.addSeries(series4);
     }
 
 }
