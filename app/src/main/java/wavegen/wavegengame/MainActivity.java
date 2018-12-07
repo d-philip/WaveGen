a31b982fe2b7d6fff53297d8bc91d56dcba30ce7
Myles-Cork <mylescork@gmail.com>
2018-12-07 13:13:40 -0500

    Changed icon. Main menu now has a rotating record label. Removed instruction activity

--- app/src/main/java/wavegen/wavegengame/MainActivity.java
+++ app/src/main/java/wavegen/wavegengame/MainActivity.java
@@ -2,15 +2,11 @@ package wavegen.wavegengame;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView record = findViewById(R.id.recordview);
+        Animation rotate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_clockwise);
+        record.startAnimation(rotate);
+
     }
 
 
