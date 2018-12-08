0e69b757d02b99354e37374b18fa7cbcd08c6bc8
Myles-Cork <mylescork@gmail.com>
2018-12-07 20:12:57 -0500

    Added score counter. Does not accurately count yet since it increments multiple times when the game is won since its in OnScroll.

--- app/src/main/java/wavegen/wavegengame/PostGame.java
+++ app/src/main/java/wavegen/wavegengame/PostGame.java
@@ -1,9 +1,11 @@
 package wavegen.wavegengame;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class PostGame extends AppCompatActivity {
 
@@ -11,6 +13,14 @@ public class PostGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_game);
+
+        SharedPreferences score = getSharedPreferences("wavescompleted", 0);
+        int wavescompleted = score.getInt("wavescompleted", 0);
+
+        String scoretext = "Total Waves: " + wavescompleted;
+
+        TextView scoreview = findViewById(R.id.scoreview);
+        scoreview.setText(scoretext);
     }
 
     public void launchPlayAgain(View view) {
